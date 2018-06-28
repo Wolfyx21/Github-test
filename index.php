7e6cc852605a49d5dda5652a3ec554124c6e9734
Emeline Tranois <emeline.tranois@laposte.net>
2018-06-28 14:12:59 +0200

    ajout background

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GITHUB test</title>
     <style>
+    body{
+        background: yellow;
+    }
         h1{
             position: absolute;
             top: 45%;
